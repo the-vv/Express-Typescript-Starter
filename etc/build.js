ed39e7d12a90ef235837b15a1a8cfc8c2a86830f
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-01-19 22:56:36 +0530

    initial commit

---
+++ etc/build.js
@@ -0,0 +1,8 @@
+const child_process = require('child_process');
+const fs = require('fs');
+
+fs.rmSync('dist', { recursive: true, force: true })
+
+child_process.execSync('tsc -p ./tsconfig.build.json --pretty')
+
+fs.cpSync('src/public', 'dist/src/public', { recursive: true })
---
+++ src/app.ts
@@ -0,0 +1,40 @@
+
+import express from 'express';
+import path from 'path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import cors from 'cors'
+import indexRouter from './routes/index';
+import usersRouter from './routes/users';
+
+class App {
+  public app: express.Application;
+
+  constructor() {
+    this.app = express();
+    this.config();
+    this.routerSetup();
+  }
+
+  private config() {
+
+    this.app.use(cors())
+    this.app.use(logger('dev'));
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: false }));
+    this.app.use(cookieParser());
+    this.app.use(express.static(path.join(__dirname, 'public')));
+  }
+
+  private routerSetup() {
+    const apiRouter = express.Router();
+    this.app.use('/api', apiRouter)
+
+    apiRouter.use('/', indexRouter);
+    apiRouter.use('/users', usersRouter);
+  }
+
+}
+
+export default new App().app;
+
